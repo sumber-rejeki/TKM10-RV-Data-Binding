1b1f320b543183277f879054db04771fccfc97d2
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-05-01 17:08:16 +0700

    first commit

---
+++ app/src/main/java/com/example/rv_databinding/Info.kt
@@ -0,0 +1,3 @@
+package com.example.rv_databinding
+
+data class Info(val image:Int, val name:String)
---
+++ app/src/main/java/com/example/rv_databinding/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.rv_databinding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.rv_databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: Adapter
+    private lateinit var list:ArrayList<Info>
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.setHasFixedSize(true)
+
+        list= ArrayList()
+        list.add(Info(R.drawable.human, "Human Book"))
+        list.add(Info(R.drawable.math, "Math Book"))
+        list.add(Info(R.drawable.psikotes, "Psikotes Book"))
+        list.add(Info(R.drawable.self, "Self Improve Book"))
+
+        adapter = Adapter(list)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
